f8bd32fb4a0f5d9d6124a597f7ed1c8acf43491a
ced <zuger.cedric@gmail.com>
2019-12-21 08:40:58 +0100

    Omniauth travis safe configuration.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,11 @@
 require 'yaml'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	id = YAML.load_file( 'config/omniauth.yaml' )
+
+  id= []
+  if File.exist? 'config/omniauth.yaml'
+    id = YAML.load_file( 'config/omniauth.yaml' )
+  end
+
 	provider :discord, id['DISCORD_CLIENT_ID'], id['DISCORD_CLIENT_SECRET']
 end
