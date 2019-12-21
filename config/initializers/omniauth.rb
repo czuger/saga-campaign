d0e186c87f3029237f8247b4929334776a95c6b6
ced <zuger.cedric@gmail.com>
2019-12-21 08:45:44 +0100

    Badges integrated.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,10 +2,9 @@ require 'yaml'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  id= []
   if File.exist? 'config/omniauth.yaml'
     id = YAML.load_file( 'config/omniauth.yaml' )
+    provider :discord, id['DISCORD_CLIENT_ID'], id['DISCORD_CLIENT_SECRET']
   end
 
-	provider :discord, id['DISCORD_CLIENT_ID'], id['DISCORD_CLIENT_SECRET']
 end
