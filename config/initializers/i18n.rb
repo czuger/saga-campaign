24b9e92b6f2c3f3c8d9e4e59acfa7436a8d7d78b
ced <zuger.cedric@gmail.com>
2020-03-10 09:06:26 +0100

    Removing webpacker and other unused stuff.

--- config/initializers/i18n.rb
+++
@@ -1,4 +0,0 @@
-I18n.config.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
-
-I18n.config.available_locales = [ :fr, :en ]
-I18n.default_locale = :fr
--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-require 'yaml'
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-
-  id= File.exist?( 'config/omniauth.yaml' ) ? YAML.load_file( 'config/omniauth.yaml' ) : {}
-  provider :discord, id['DISCORD_CLIENT_ID'], id['DISCORD_CLIENT_SECRET']
-
-end
