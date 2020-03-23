23c03adf84ff464c133d61ee71036d7a8df05e49
ced <zuger.cedric@gmail.com>
2020-03-23 19:57:27 +0100

    Maintenance costs are shown in controlled points.

---
+++ app/helpers/campaigns_helper.rb
@@ -0,0 +1,19 @@
+module CampaignsHelper
+
+  MAINTENANCE = { 6 => 1, 12 => 2, 18 => 3, 24 => 5, 30 => 7, 36 => 10, 42 => 20, 48 => 30 }
+
+  def compute_maintenance( player )
+    points = player.gangs.map{ |g| g.points }.inject( &:+ )
+
+    m = MAINTENANCE.keys.sort.dup
+
+    return MAINTENANCE[ m.last ] if points > m.last
+
+    while m.count >= 2
+      base = m.shift
+      return MAINTENANCE[ base ] if points >= base && points < m.first
+    end
+
+    0
+  end
+end
