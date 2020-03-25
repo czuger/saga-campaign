91d7f3fecd4535cb9d693f81066742e8574d60bf
ced <zuger.cedric@gmail.com>
2020-03-25 19:48:39 +0100

    Show fight log improved.

--- app/helpers/fights_helper.rb
+++ app/helpers/fights_helper.rb
@@ -16,7 +16,7 @@ module FightsHelper
   def pass_detail( pass_detail )
     attacker_name = Unit.short_name_from_log_data( pass_detail.attacker )
 
-    "L'unité #{attacker_name} n'attaquera pas ce tour (#{pass_detail.attack_trigger}% de chances d'attaquer)."
+    t( '.pass_details_string', name: attacker_name, attack_trigger: pass_detail.attack_trigger )
   end
 
   def fight_detail( fight_detail )
@@ -25,24 +25,25 @@ module FightsHelper
       attack_detail_string_dice_pool_part = t( '.attack_detail_string_dice_pool_part', count: fight_detail.dice_pool )
       attack_detail_string_hits_part = t( '.attack_detail_string_hits_part', count: fight_detail.hits )
 
-      attack_translation = fight_detail.attack_phase == :attack ? '.attack_detail_string' : '.retailation_detail_string'
+      attack_phase = t( '.attack_phase.' + fight_detail.attack_phase.to_s )
 
-      attacker_name = Unit.long_name_from_log_data( fight_detail.attacker )
-      defender_name = Unit.long_name_from_log_data( fight_detail.defender )
+      attacker_name = Unit.short_name_from_log_data( fight_detail.attacker )
+      defender_name = Unit.short_name_from_log_data( fight_detail.defender )
 
-      attack = t( attack_translation, attacker_name: attacker_name, defender_name: defender_name,
+      attack = t( '.attack_detail_string', attacker_name: attacker_name, defender_name: defender_name,
                   attack_type_name: attack_type_name(fight_detail.attack_type),
                   dice_pool_part: attack_detail_string_dice_pool_part,
                   opponent_armor: fight_detail.opponent_armor,
                   hits_part: attack_detail_string_hits_part,
-                  count: fight_detail.attacker.amount,
-                  hits_rolls: fight_detail.hits_rolls)
+                  attacker_count: fight_detail.attacker.amount, defender_count: fight_detail.defender.amount,
+                  hits_rolls: fight_detail.hits_rolls, attack_phase: attack_phase )
 
       defend_detail_string_hits_part = t( '.attack_detail_string_hits_part', count: fight_detail.damages )
+      defend_detail_string_saves_part = t( '.defend_detail_string_saves_part', count: fight_detail.saves )
+
       defense = t( '.defend_detail_string', defender_name: defender_name, opponent_save: fight_detail.opponent_save,
-                   saves_rolls: fight_detail.saves_rolls, saves: fight_detail.saves,
-                   hits_part: defend_detail_string_hits_part,
-                   count: fight_detail.defender.amount )
+                   saves_rolls: fight_detail.saves_rolls, saves_string: defend_detail_string_saves_part,
+                   hits_part: defend_detail_string_hits_part )
 
       attack.upcase_first + ' ' + defense.upcase_first
     end
@@ -54,11 +55,18 @@ module FightsHelper
     hit_detail.each do |hit|
       name = Unit.short_name_from_log_data( hit.unit )
 
+      defend_detail_string_hits_part = t( '.attack_detail_string_hits_part', count: hit.hits )
+
       if hit.type == :protection
-        r << "L'unité #{name} a une protection. Elle prend #{hit.hits} touches et sa protection passe de #{hit.before} à #{hit.after}."
+        r << t( '.hit_protection_string', name: name, hits: hit.hits, before: hit.before, after: hit.after,
+                hits_part: defend_detail_string_hits_part )
       else
-        r << "L'unité #{name} prend #{hit.hits} touches, perds #{hit.damages} figurines et passe de #{hit.before} à #{hit.after}."
-        r << "L'unité #{name} est détruite." if hit.unit_destroyed
+        defend_detail_string_casualties_part = t( '.defend_detail_string_casualties_part', count: hit.damages )
+
+        r << t( '.hit_loss_string', name: name, hits: hit.hits, damages: hit.damages, before: hit.before,
+                after: hit.after, hits_part: defend_detail_string_hits_part, casualties_part: defend_detail_string_casualties_part )
+        # r << "L'unité #{name} prend #{hit.hits} touches, perds #{hit.damages} figurines et passe de #{hit.before} à #{hit.after}."
+        r << t( '.hit_destroyed_string', name: name ) if hit.unit_destroyed
       end
     end
 
