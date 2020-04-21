146daff6adc2f9da9f74d2f1592898dd4e82a02f
ced <zuger.cedric@gmail.com>
2020-04-21 18:00:13 +0200

    Added a file to create database user.

---
+++ work/create_db_user.sql
@@ -0,0 +1,12 @@
+/* run with psql -h localhost -p 5434 < create_db_user.sql */
+/* of course, on production change the password and delete the file */
+
+CREATE ROLE saga_campaign WITH
+LOGIN
+NOSUPERUSER
+CREATEDB
+NOCREATEROLE
+INHERIT
+NOREPLICATION
+CONNECTION LIMIT -1
+PASSWORD 'xxxxxx';
