a98b30b7eae7d96ecc265054dfbef704e4cd4bda
ced <zuger.cedric@gmail.com>
2020-04-23 18:44:47 +0200

    Log auth hash because of strange issues + show user name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   def self.find_or_create_from_auth_hash(auth_hash)
 
-    # pp auth_hash
+    pp auth_hash
 
     User.find_or_create_by!( provider: auth_hash['provider'], uid: auth_hash['uid'] ) do |user|
       user.name = auth_hash['info']['name']
