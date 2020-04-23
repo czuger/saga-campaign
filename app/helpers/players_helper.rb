c00e3c434b7245db9e96609e1606623dce8ee334
ced <zuger.cedric@gmail.com>
2020-04-23 13:16:31 +0200

    Too weak gangs can't move outside base.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -10,4 +10,9 @@ module PlayersHelper
       gang.movements[movement_number]
     end
   end
+
+  # If the gang is at the base and has less than 6 points it can't move.
+  def gang_cant_move?( gang )
+    GameRules::Factions.recruitment_positions( @player ).include?( gang.location ) && gang.points < 6
+  end
 end
