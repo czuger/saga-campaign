080d909def3c20bf3db0f5323203ef27da438efd
ced <zuger.cedric@gmail.com>
2020-04-28 13:37:03 +0200

    Error fixed in movement screen.

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -88,7 +88,7 @@ class CampaignsController < ApplicationController
 
   def show_movements
     @movements = @campaign.movements_results.includes( :gang, { player: :user } ).order( :id )
-    @gangs = @campaign.gangs.joins( :player ).includes( { player: :user } ).order( 'players.initiative', :movement_order )
+    @gangs = @campaign.gangs.where.not( movement_order: nil ).joins( :player ).includes( { player: :user } ).order( 'players.initiative', :movement_order )
   end
 
   def show_victory_status
