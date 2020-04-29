8480cb529ac1b28fd3a5caeccdfd592e4ef8b782
ced <zuger.cedric@gmail.com>
2020-04-29 20:57:30 +0200

    Movement limit lowered to 4.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -11,8 +11,8 @@ module PlayersHelper
     end
   end
 
-  # If the gang is at the base and has less than 6 points it can't move.
+  # If the gang is at the base and has less than 4 points it can't move.
   def gang_cant_move?( gang )
-    GameRules::Factions.recruitment_positions( @player ).include?( gang.location ) && gang.points < 6
+    GameRules::Factions.recruitment_positions( @player ).include?( gang.location ) && gang.points < 4
   end
 end
