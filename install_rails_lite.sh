fe9e412d711385dd191d01a21600c9220346b4a0
ced <zuger.cedric@gmail.com>
2020-06-18 17:08:52 +0200

    Show is protected from "none" icon.

---
+++ install_rails_lite.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rails new daily-schedule --skip-action-mailer --skip-action-mailbox --skip-action-text --skip-active-storage --skip-action-cable --skip-bootsnap --skip-bundle --skip-webpack-install --skip-javascript --skip
